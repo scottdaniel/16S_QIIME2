6626574dfd4cb741a10ae381345957dde4d69286
Scott G. Daniel <danielsg@email.chop.edu>
2021-12-07 13:24:33 -0500

    picrust works now

--- run_snakemake.bash
+++ run_snakemake.bash
@@ -6,10 +6,9 @@
 #SBATCH --mail-user=danielsg@chop.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --no-requeue
-#SBATCH -t 12:00:00
+#SBATCH -t 72:00:00
 #SBATCH --output=slurm_%x_%j.out
 
-#Uncomment the next two lines if you want to 'qsub' this script
 source ~/.bashrc.conda #needed to make "conda" command to work
 conda activate qiime2-snakemake
 
@@ -32,4 +31,4 @@ snakemake \
     --notemp \
     --printshellcmds \
     --cluster \
-    "sbatch --account={cluster.account} --partition={cluster.partition} --mem-per-cpu={cluster.memcpu} --cpus-per-task={threads} --time={cluster.time} --job-name={cluster.name} --output=slurm_%x_%j.out"
+    "sbatch --account={cluster.account} --partition={cluster.partition} --mem={cluster.mem} --cpus-per-task={threads} --time={cluster.time} --job-name={cluster.name} --output=slurm_%x_%j.out"
