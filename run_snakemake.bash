b7da8d4cd07f02ef19f17916f1ade9834c734863
Scott G. Daniel <danielsg@email.chop.edu>
2023-03-03 17:55:53 -0500

    mem is working threads need fixing

--- run_snakemake.bash
+++ run_snakemake.bash
@@ -9,14 +9,14 @@
 #SBATCH -t 72:00:00
 #SBATCH --output=slurm_%x_%j.out
 
+if [[ ! -f ./config.yaml ]]; then
+    echo "Must have a config.yaml to be able to run"
+    exit 1
+fi
+
 source ~/.bashrc.conda #needed to make "conda" command to work
 conda activate qiime2-2023.2
 
 set -xeuo pipefail
-
-if [[ -f config.yaml ]]; then
-    echo "Must have a config.yaml to be able to run"
-    exit 1
-fi
 
 snakemake --profile ./
